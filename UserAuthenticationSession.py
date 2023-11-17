66224956ac931d6823e0e7d375192d401eb45a48
s-okal <145070456+s-okal@users.noreply.github.com>
2023-11-16 21:04:50 -0500

    bug fixes - user authentication
    Played around with UserAuthentication a bit... I will make additional revisions over the next day or so and possibly consolidate UserAuthentication and UserAuthenticationSession classes

--- UserAuthenticationSession.py
+++ UserAuthenticationSession.py
@@ -1,4 +1,5 @@
 from enum import Enum
+# import UserAuthentication
 
 class AuthenticationLevel(Enum):
     # two different user types
@@ -7,21 +8,27 @@ class AuthenticationLevel(Enum):
 
 
 class UserAuthenticationSession:
+
     def __init__(self):
         self.id = 0 # default value for user id
         self.isAuthenticated = False # default for authentication state
         self.authenticationLevel = None # 
 
     def tryAuthentication(self, username, password):
-        if username and password: # if username and password match
-            self.isAuthenticated = True # session is authenticated
-            if username == "admin" and password == "suchaweakpassword1":
-                # if an admin user, use enumerated type to specify
-                self.authenticationLevel = AuthenticationLevel.admin
-            elif username == 'johndoe' and password == 'weakerpassword1':
-                # other standard user authentication.
-                self.authenticationLevel = AuthenticationLevel.user 
-                # NOTE: improve later. Perhaps use a dicitonary or list to store user creds
+        # NOTE: improve later. Perhaps use a dicitonary or list to store user creds
+        if username == "admin" and password == "suchaweakpassword1":
+            # if an admin user, use enumerated type to specify
+            self.authenticationLevel = AuthenticationLevel.admin
+            # admin authenticated
+            self.isAuthenticated = True
+            print('admin')
+        elif username == 'johndoe' and password == 'weakerpassword1':
+            # other standard user authentication.
+            self.authenticationLevel = AuthenticationLevel.user 
+            # johndoe (user) authenticated
+            self.isAuthenticated = True
+            # return self.isAuthenticated
+            print('user')
         else:
             # if username and password are not found, authenticaiton will fail
             self.isAuthenticated = False
